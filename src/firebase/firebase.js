12d96cf1cc2a0cb344a06ef0ab0c3794bddc45dc
valeriya-laktionova <valeriya.laktionova@stud.esdc.lt>
2025-05-15 21:09:42 +0300

    fix: fixed keys

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+console.log("API Key from .env:", import.meta.env.VITE_API_KEY);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDBxEMNaOaJ6-rA85TM-UAi-OkQQ85X9oI",
-  authDomain: "deliveryapp-3e263.firebaseapp.com",
-  projectId: "deliveryapp-3e263",
-  storageBucket: "deliveryapp-3e263.firebasestorage.app",
-  messagingSenderId: "1001322335726",
-  appId: "1:1001322335726:web:7266ac065ac0af9b7bd2ff",
-  measurementId: "G-8DKY2JK97M",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
